1b4dbef38b66e3be07f4e9874391aa56eb46c904
NishiOwO <nishi@nishi.boats>
2025-03-26 17:30:48 +0900

    add some init log

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+#include <nb_version.h>
+
 #include <stdio.h>
 
-int main(int argc, char** argv) { return 0; }
+int main(int argc, char** argv) {
+	nb_version_t ver;
+	nb_get_version(&ver);
+	printf("NishBox engine %s - Powered by Pawn and ODE\n", ver.full);
+
+	return 0;
+}
