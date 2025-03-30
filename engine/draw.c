48f81212ee21dbb4ac2c50dbb040d850b29f4f08
NishiOwO <nishi@nishi.boats>
2025-03-30 11:28:59 +0900

    remove triangle

--- engine/draw.c
+++ engine/draw.c
@@ -78,12 +78,6 @@ void nb_draw_end_2d(nb_draw_t* draw) {
 void nb_draw_frame(nb_draw_t* draw) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	nb_draw_begin_2d(draw);
-	glColor3f(1, 0, 0);
-	glBegin(GL_TRIANGLES);
-	glVertex2d(0, draw->height);
-	glVertex2d(draw->width / 2, 0);
-	glVertex2d(draw->width, draw->height);
-	glEnd();
 	nb_draw_end_2d(draw);
 }
 
