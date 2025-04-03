b785cfd453f7684f8a5127a98d777ac7ce4ed8d8
NishiOwO <nishi@nishi.boats>
2025-04-03 20:47:49 +0900

    fix

--- mk/p_default.mk
+++ mk/p_default.mk
@@ -2,10 +2,10 @@ ifeq ($(PLATFORM),)
 BACKEND = GLX
 
 ODE_CFLAGS = `pkg-config --cflags ode`
-ODE_LIBS = `pkg-config --libs ode`
+ODE_LIBS = `pkg-config --libs ode` -lstdc++
 ifeq ($(BACKEND),GLX)
-GL_CFLAGS = `pkg-config --cflags gl` `pkg-config --cflags glu`
-GL_LIBS = `pkg-config --libs gl` `pkg-config --libs glu`
+GL_CFLAGS = `pkg-config --cflags x11` `pkg-config --cflags gl` `pkg-config --cflags glu`
+GL_LIBS = `pkg-config --libs x11` `pkg-config --libs gl` `pkg-config --libs glu`
 endif
 ifeq ($(BACKEND),GLFW)
 GL_CFLAGS = `pkg-config --cflags glfw3` `pkg-config --cflags glu`
