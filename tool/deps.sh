ae48c2dee2b48674ac8e6e1fd0be15ec51c59031
Nishi <nishi@nishi.boats>
2025-04-11 17:04:23 +0900

    fixing

--- tool/deps.sh
+++ tool/deps.sh
@@ -6,8 +6,8 @@ OBJDUMP=${OBJDUMP:-${GCC_PREFIX}objdump}
 solve () {
 	path=""
 	$OBJDUMP -p $1 | grep "DLL Name:" | rev | cut -d" " -f1 | rev | while read a; do
-		if [ -e "./deps/bin/$a" ]; then
-			path="./deps/bin/$a"
+		if [ -e "./engine/lib/*/*/$a" ]; then
+			path="./engine/lib/*/*/$a"
 		else
 			path="`$CC -print-file-name=$a`"
 		fi
--- tool/pack.sh
+++ tool/pack.sh
@@ -5,7 +5,7 @@ rm -rf nishbox.zip
 mkdir -p nishbox/bin
 mkdir -p nishbox/lib
 cp `./tool/deps.sh` ./nishbox/bin/
-cp src/nishbox.exe ./nishbox/bin/
+cp bin/*/*/nishbox.exe ./nishbox/bin/
 ${STRIP} ./nishbox/bin/*.dll ./nishbox/bin/*.exe
 zip -rv nishbox.zip nishbox
 rm -rf nishbox
