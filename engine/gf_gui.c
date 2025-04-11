847424f6e725c09c4ba96d3a8ec46328ad2b915e
Nishi <nishi@nishi.boats>
2025-04-11 17:17:16 +0900

    add gui

---
+++ engine/gf_gui.c
@@ -0,0 +1,10 @@
+#include <gf_pre.h>
+
+/* External library */
+
+/* Interface */
+#include <gf_gui.h>
+
+/* Engine */
+
+/* Standard */
---
+++ engine/include/gf_gui.h
@@ -0,0 +1,13 @@
+#ifndef __GF_GUI_H__
+#define __GF_GUI_H__
+
+#include <gf_pre.h>
+#include <gf_macro.h>
+
+/* Type */
+
+/* Engine */
+
+/* Standard */
+
+#endif
