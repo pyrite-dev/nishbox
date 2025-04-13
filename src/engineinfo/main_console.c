e162ecd71c9b91a577587f9fc4ce89190cf6e8b5
NishiOwO <nishi@nishi.boats>
2025-04-13 20:14:43 +0900

    fix console one a little bit

--- src/engineinfo/main_console.c
+++ src/engineinfo/main_console.c
@@ -10,9 +10,10 @@ int main(int argc, char** argv) {
 	gf_version_t ver;
 	gf_get_version(&ver);
 
-	printf("GoldFish Engine version: %s\n", ver.full);
-	printf("Thread model: %s\n", ver.thread);
-	printf("Renderer: %s on %s\n", ver.driver, ver.backend);
+	printf("GoldFish Engine %s\n", ver.full);
+	printf("Build Date   : %s\n", ver.date);
+	printf("Thread model : %s\n", ver.thread);
+	printf("Renderer     : %s on %s\n", ver.driver, ver.backend);
 
 	return 0;
 }
