faf42d897add3b27563893b93268bb1bc748d582
NishiOwO <nishi@nishi.boats>
2025-04-15 16:51:30 +0900

    fix doxygen config

--- engine/include/gf_core.h
+++ engine/include/gf_core.h
@@ -1,8 +1,7 @@
 /**
- * Core of the engine
- *
- * Including this should (will) be enough for usual case,
- * at least in the future.
+ * @file gf_core.h
+ * @~english
+ * @brief Core
  */
 
 #ifndef __GF_CORE_H__
@@ -19,11 +18,46 @@
 
 /* Standard */
 
+/**
+ * @~english
+ * @brief Initialize engine
+ */
 GF_EXPORT void	       gf_engine_begin(void);
+
+/**
+ * @~english
+ * @brief Cleanup engine
+ */
 GF_EXPORT void	       gf_engine_end(void);
+
+/**
+ * @~english
+ * @brief Create engine instance
+ * @param title Title to be shown on the window
+ * @param nogui `0` to enable client-part, otherwise disable client-part
+ */
 GF_EXPORT gf_engine_t* gf_engine_create(const char* title, int nogui);
+
+/**
+ * @~english
+ * @brief Start engine main loop
+ * @param engine Engine instance
+ */
 GF_EXPORT void	       gf_engine_loop(gf_engine_t* engine);
+
+/**
+ * @~english
+ * @brief Set user-drawing callback
+ * @param engine Engine instance
+ * @param func Callback
+ */
 GF_EXPORT void	       gf_engine_set_draw(gf_engine_t* engine, void (*func)(gf_draw_t*));
+
+/**
+ * @~english
+ * @brief Destroy engine instance
+ * @param engine Engine instance
+ */
 GF_EXPORT void	       gf_engine_destroy(gf_engine_t* engine);
 
 #endif
--- engine/include/gf_type/core.h
+++ engine/include/gf_type/core.h
@@ -1,3 +1,9 @@
+/**
+ * @file gf_type/core.h
+ * @~english
+ * @brief Type definitions related to core
+ */
+
 #ifndef __GF_TYPE_CORE_H__
 #define __GF_TYPE_CORE_H__
 
@@ -14,6 +20,19 @@
 /* Standard */
 #include <stdio.h>
 
+/**
+ * @struct gf_engine_t
+ * @~english
+ * @brief Engine instance
+ *
+ * @var gf_engine_t::physics
+ * @brief Physics part
+ * @todo Create gf_server_t and move physics there
+ *
+ * @var gf_engine_t::draw
+ * @brief Draw part
+ * @todo Create gf_client_t and move draw there
+ */
 GF_DECLARE_TYPE(engine, {
 	gf_physics_t* physics;
 	gf_draw_t*    draw;
