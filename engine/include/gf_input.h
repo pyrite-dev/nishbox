023b4d257e3c719718b1041b321800a81ac5e2ce
NishiOwO <nishi@nishi.boats>
2025-04-17 22:06:24 +0900

    add input template

---
+++ engine/include/gf_input.h
@@ -0,0 +1,19 @@
+/**
+ * @file gf_input.h
+ * @~english
+ * @brief Input
+ */
+
+#ifndef __GF_INPUT_H__
+#define __GF_INPUT_H__
+
+#include <gf_pre.h>
+#include <gf_macro.h>
+
+/* Type */
+
+/* Engine */
+
+/* Standard */
+
+#endif
