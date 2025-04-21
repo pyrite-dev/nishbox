be122576ac4cc8fb73c6087bfada0dde0c2d6a7b
NishiOwO <nishi@nishi.boats>
2025-04-21 21:50:13 +0900

    fix and move

--- tool/respack.sh
+++ tool/respack.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-PACK="music font texture"
 if [ "x${WINE}" = "x" ]; then
-	exec ./engine/bin/*/*/pack base.pak ${PACK}
+	exec ./engine/bin/*/*/pack -d data base.pak
 else
-	exec ${WINE} ./engine/bin/*/*/pack.exe base.pak ${PACK}
+	exec ${WINE} ./engine/bin/*/*/pack.exe -d data base.pak
 fi
