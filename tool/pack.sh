4348218553720b04096d0ee43511543fbc457071
NishiOwO <nishi@nishi.boats>
2025-04-21 22:04:51 +0900

    fix

--- tool/pack.sh
+++ tool/pack.sh
@@ -2,15 +2,30 @@
 
 rm -rf nishbox
 rm -rf nishbox.zip
-mkdir -p nishbox/bin/
-mkdir -p nishbox/lib/
-mkdir -p nishbox/include/
+mkdir -p nishbox
+mkdir -p nishbox/bin
+mkdir -p nishbox/lib
+mkdir -p nishbox/include
+
+cat > nishbox/README.txt << EOF
+ - GoldFish DLL
+ - NishBox executables
+ - GoldFish executables
+are in bin/
+
+ - GoldFish headers
+is in include/
+
+ - GoldFish .lib
+is in lib/
+EOF
+
 cp -rf engine/include/* nishbox/include/
 cp engine/lib/*/*/*.lib nishbox/lib/
 cp bin/*/*/*.exe ./nishbox/bin/
 cp engine/bin/*/*/*.exe ./nishbox/bin/
 cp engine/lib/*/*/*.dll ./nishbox/bin/
-cp base.pak ./nishbox/
+cp base.pak ./nishbox/bin/
 zip -zrv nishbox.zip nishbox << EOF
 +---------------------------------------+
 | NishBox/GoldFish distribution zipfile |
