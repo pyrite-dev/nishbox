3a5cdf8bf0d607d5adf6c368545849e4fecbbe0e
NishiOwO <nishi@nishi.boats>
2025-04-21 10:29:22 +0900

    update clean.sh

--- tool/clean.sh
+++ tool/clean.sh
@@ -2,5 +2,13 @@
 rm -rf obj lib bin *.make Makefile
 cd engine
 rm -rf obj lib bin *.make Makefile
+
+cd src
+rm -rf obj lib bin *.make Makefile
+cd ..
+
 cd util
 rm -rf obj lib bin *.make Makefile
+cd ..
+
+cd ..
