9688764f676fe84df2eca491f05c613f16a77889
NishiOwO <nishi@nishi.boats>
2025-04-21 22:01:36 +0900

    update zipfile generator

--- tool/pack.sh
+++ tool/pack.sh
@@ -2,10 +2,24 @@
 
 rm -rf nishbox
 rm -rf nishbox.zip
-mkdir -p nishbox/
-cp bin/*/*/*.exe ./nishbox/
-cp engine/bin/*/*/*.exe ./nishbox/
-cp engine/lib/*/*/*.dll ./nishbox/
+mkdir -p nishbox/bin/
+mkdir -p nishbox/lib/
+mkdir -p nishbox/include/
+cp -rf engine/include/* nishbox/include/
+cp engine/lib/*/*/*.lib nishbox/lib/
+cp bin/*/*/*.exe ./nishbox/bin/
+cp engine/bin/*/*/*.exe ./nishbox/bin/
+cp engine/lib/*/*/*.dll ./nishbox/bin/
 cp base.pak ./nishbox/
-zip -rv nishbox.zip nishbox
+zip -zrv nishbox.zip nishbox << EOF
++---------------------------------------+
+| NishBox/GoldFish distribution zipfile |
+| Contains:                             |
+|    1. NishBox executables             |
+|    2. GoldFish executables            |
+|    3. GoldFish development kit        |
+|                                Enjoy! | 
++---------------------------------------+
+.
+EOF
 rm -rf nishbox
