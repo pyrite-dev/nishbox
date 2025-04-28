454eefa1ac513a636be7d7e99eb556b812ad093f
NishiOwO <nishi@nishi.boats>
2025-04-28 17:00:21 +0900

    do that on miniaudio too

---
+++ engine/external/miniaudio/README.md
@@ -0,0 +1,3 @@
+miniaudio repository is quite big, so this directory only contains the required headers/sources.
+
+See https://github.com/mackron/miniaudio .
---
+++ engine/external/miniaudio/get.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -L https://github.com/mackron/miniaudio/raw/refs/heads/master/miniaudio.c > miniaudio.c
+curl -L https://github.com/mackron/miniaudio/raw/refs/heads/master/miniaudio.h > miniaudio.h
