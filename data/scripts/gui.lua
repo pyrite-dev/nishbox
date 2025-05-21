44d3bb81721516d84650b47259404fd34210d398
NishiOwO <nishi@nishi.boats>
2025-05-22 02:59:45 +0900

    fix and update

--- data/scripts/gui.lua
+++ data/scripts/gui.lua
@@ -160,8 +160,9 @@ local function menu()
 	for i,v in ipairs(menu_entries) do
 		if v.name then
 			local size = 56
-			local btn = gf.gui.create("button", 5 + size / 3, 5 + size * (i - 1), gf.graphic.text_width(bold_font, size, v.name) + 5, size)
+			local btn = gf.gui.create("button", 5 + size / 3, 5 + size * (i - 1), gf.graphic.text_width(font, size, v.name) + 5, size)
 			btn:set_text(v.name)
+			btn:font(font)
 			btn:prop("integer", "y-base", 1)
 			btn:prop("floating", "font-size", size)
 			btn:prop("integer", "no-border", 1)
