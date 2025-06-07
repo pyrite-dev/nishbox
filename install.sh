8f20c8f782bfe4aac878768f57cf8e997d4b6a1f
NishiOwO <nishi@nishi.boats>
2025-06-08 00:53:46 +0900

    things

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-mkdir -p /usr/local/lib
-mkdir -p /usr/local/bin
-mkdir -p /usr/local/share/nishbox
-cp ./engine/lib/*/*/*.so /usr/local/lib/ >/dev/null 2>&1 || echo 'Failed to copy engine, but it is okay'
-cp ./bin/*/*/nishbox /usr/local/bin/
-cp ./base.pak /usr/local/share/nishbox/
-echo 'Installation success'
--- src/nishbox.c
+++ src/nishbox.c
@@ -18,7 +18,27 @@
 
 gf_engine_t* engine = NULL;
 
-#ifndef _WIN32
+#ifdef _WIN32
+char* get_registry(const char* main, const char* sub){
+	DWORD bufsize = 512;
+	HKEY handle;
+	char* value = malloc(513);
+	int err = RegOpenKeyEx(HKEY_LOCAL_MACHINE, main, 0, KEY_QUERY_VALUE, &handle);
+	if(err = ERROR_SUCCESS){
+		err = RegQueryValueEx(handle, sub, NULL, NULL, value, &bufsize);
+		if(err != ERROR_SUCCESS){
+			free(value);
+			RegCloseKey(handle);
+			return NULL;
+		}
+		RegCloseKey(handle);
+		return value;
+	}else{
+		free(value);
+		return NULL;
+	}
+}
+#else
 void handle_signal(int sig) {
 	if(sig == SIGINT) {
 		if(engine != NULL) {
@@ -33,6 +53,9 @@ int main(int argc, char** argv) {
 	FILE*	     f;
 	int	     nogui = 0;
 	int	     i;
+#ifdef _WIN32
+	char* regpath = get_registry("Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\NishBox", "InstallDir");
+#endif
 
 	for(i = 1; i < argc; i++) {
 		if(strcmp(argv[i], "-dedicated") == 0) {
@@ -50,7 +73,7 @@ int main(int argc, char** argv) {
 	}
 #endif
 	gf_engine_begin();
-#if 0
+#ifdef DEBUG
 	engine = gf_engine_create_ex("NishBox", nogui, "data", argv, argc);
 #else
 	f = fopen("base.pak", "r");
@@ -66,6 +89,15 @@ int main(int argc, char** argv) {
 		gf_engine_end();
 		return 1;
 	}
+#ifdef _WIN32
+	if(regpath == NULL){
+		gf_engine_prefix(engine, PREFIX);
+	}else{
+		gf_engine_prefix(engine, regpath);
+	}
+#else
+	gf_engine_prefix(engine, PREFIX "/share");
+#endif
 	gf_engine_name(engine, "NishBox");
 	gf_engine_loop(engine);
 	gf_engine_destroy(engine);
@@ -73,6 +105,7 @@ int main(int argc, char** argv) {
 
 #ifdef _WIN32
 	if(gf_log_default != NULL && !nogui) fclose(gf_log_default);
+	if(regpath != NULL) free(regpath);
 #endif
 
 	return 0;
